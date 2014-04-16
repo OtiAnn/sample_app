1e63688fbc0a1bab9d2b3c4615aa1a72632342e3
Annie Chen <anniebh30@gmail.com>
2014-04-16 08:20:05 +0000

    Helper Practice

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,5 @@
 module StaticPagesHelper
+  def youtube (aaaaa)
+    %Q(<iframe width="420" height="315" src="//www.youtube.com/embed/#{aaaaa}" frameborder="0" allowfullscreen></iframe>)
+  end
 end
