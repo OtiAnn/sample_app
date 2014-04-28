53d4b0bdad81fb12d6e235bdad8fc5d07da346d3
OtiAnn <anniebh30@gmail.com>
2014-04-28 18:31:18 +0800

    Member list finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,61 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
-  def show
+  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
+    def index
+      @users = User.paginate(page: params[:page])
+    end
+
+    def show
+      @user = User.find(params[:id])
+    end
+    def new
+      @user = User.new
+    end
+
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        sign_in @user
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to @user
+      else
+        render 'new'
+      end
+    end
+    
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
     @user = User.find(params[:id])
-  end
- def create
-    @user = User.new(user_params)
-    if @user.save
-      sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
       redirect_to @user
     else
-      render 'new'
+      render 'edit'
     end
   end
 
-  private
+    private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+
+    # Before filters
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+
 end
